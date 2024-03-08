9ea32d72691e30636929fecd1b43d402de5124d1
Kazuya Takei <myself@attakei.net>
2024-03-08 11:35:46 +0900

    feat: Display version as subcommand 'version'

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
 // Entry point.
+use clap::{Parser, Subcommand};
+
 const VERSION: &'static str = "0.0.0";
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Display version information
+    Version {},
+}
+
 fn main() {
-    println!("Running gazer v{VERSION}");
+    let cli = Cli::parse();
+    match &cli.command {
+        Some(Commands::Version {}) => {
+            println!("Running gazer v{VERSION}");
+        }
+        None => {}
+    }
 }
