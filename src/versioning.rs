8796e62ce170e61929f3d7e925080aaca9e3262c
Kazuya Takei <myself@attakei.net>
2024-03-11 03:55:50 +0900

    refactor: Add unit tests

--- src/versioning.rs
+++ src/versioning.rs
@@ -29,3 +29,35 @@ pub fn up_patch(base: &Version) -> Version {
         build: BuildMetadata::EMPTY,
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_up_major() {
+        let before = Version::parse("1.2.3").unwrap();
+        let after = up_major(&before);
+        assert_eq!(after.major, before.major + 1);
+        assert_eq!(after.minor, 0);
+        assert_eq!(after.patch, 0);
+    }
+
+    #[test]
+    fn test_up_minor() {
+        let before = Version::parse("1.2.3").unwrap();
+        let after = up_minor(&before);
+        assert_eq!(after.major, before.major);
+        assert_eq!(after.minor, before.minor + 1);
+        assert_eq!(after.patch, 0);
+    }
+
+    #[test]
+    fn test_up_patch() {
+        let before = Version::parse("1.2.3").unwrap();
+        let after = up_patch(&before);
+        assert_eq!(after.major, before.major);
+        assert_eq!(after.minor, before.minor);
+        assert_eq!(after.patch, before.patch + 1);
+    }
+}
