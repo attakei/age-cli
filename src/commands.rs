1bf9dacb2cb46126b0c2a5268871b4c51849ddb6
Kazuya Takei <myself@attakei.net>
2024-03-17 09:57:26 +0900

    refactor: Move for responsibility

--- src/commands.rs
+++ src/commands.rs
@@ -1,7 +1,50 @@
-pub mod info;
-pub mod init;
-pub mod major;
-pub mod minor;
-pub mod patch;
-pub mod update;
-pub mod version;
+mod info;
+mod init;
+mod major;
+mod minor;
+mod patch;
+mod update;
+mod version;
+
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+use semver::Version;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Display config.
+    Info {},
+    /// Update version text
+    Update { new_version: Version },
+    /// Shortcut of major version update
+    Major {},
+    /// Shortcut of minor version update
+    Minor {},
+    /// Shortcut of patch version update
+    Patch {},
+    /// Display version information
+    Version {},
+    /// Create configuration file.
+    Init {},
+}
+
+pub fn execute() -> Result<()> {
+    let cli = Cli::parse();
+    match &cli.command {
+        Some(Commands::Info {}) => info::execute(),
+        Some(Commands::Version {}) => version::execute(),
+        Some(Commands::Init {}) => init::execute(),
+        Some(Commands::Update { new_version }) => update::execute(new_version),
+        Some(Commands::Major {}) => major::execute(),
+        Some(Commands::Minor {}) => minor::execute(),
+        Some(Commands::Patch {}) => patch::execute(),
+        None => Ok(()),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,46 +3,10 @@ mod app;
 mod commands;
 mod config;
 mod versioning;
-use clap::{Parser, Subcommand};
-use semver::Version;
-
-#[derive(Parser)]
-#[command(version, about, long_about = None)]
-struct Cli {
-    #[command(subcommand)]
-    command: Option<Commands>,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// Display config.
-    Info {},
-    /// Update version text
-    Update { new_version: Version },
-    /// Shortcut of major version update
-    Major {},
-    /// Shortcut of minor version update
-    Minor {},
-    /// Shortcut of patch version update
-    Patch {},
-    /// Display version information
-    Version {},
-    /// Create configuration file.
-    Init {},
-}
+use crate::commands::execute;
 
 fn main() {
-    let cli = Cli::parse();
-    let result = match &cli.command {
-        Some(Commands::Info {}) => commands::info::execute(),
-        Some(Commands::Version {}) => commands::version::execute(),
-        Some(Commands::Init {}) => commands::init::execute(),
-        Some(Commands::Update { new_version }) => commands::update::execute(new_version),
-        Some(Commands::Major {}) => commands::major::execute(),
-        Some(Commands::Minor {}) => commands::minor::execute(),
-        Some(Commands::Patch {}) => commands::patch::execute(),
-        None => Ok(()),
-    };
+    let result = execute();
     match result {
         Ok(()) => 0,
         Err(err) => {
