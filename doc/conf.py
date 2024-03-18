2b83a92d7a755038c74eb780fdbb62521bd0f8ef
Kazuya Takei <myself@attakei.net>
2024-03-19 01:49:59 +0900

    doc: Init document env

---
+++ doc/conf.py
@@ -0,0 +1,18 @@
+# noqa: D100
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+project = "age"
+copyright = "2024, attakei.net"
+author = "Kazuya Takei"
+release = "0.3.0"
+
+# -- General configuration
+extensions = []
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output
+html_theme = "haiku"
+html_static_path = ["_static"]
+html_title = "age"
