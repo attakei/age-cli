6168251d39769ca664499485802cdfe210484a17
Kazuya Takei <myself@attakei.net>
2024-03-19 01:06:54 +0900

    ci: Implement test for 'age init'

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,6 +1,11 @@
 """Test case for ``age init``."""
 
+from pathlib import Path
+from subprocess import PIPE, run
 
-def test_it():
-    """This is stub of test to pass pytest."""
-    pass
+
+def test_it(target_bin: str, tmp_path: Path):
+    """'init' command generate config file."""
+    proc = run([target_bin, "init"], stdout=PIPE, stderr=PIPE, text=True, cwd=tmp_path)
+    assert proc.returncode == 0
+    assert (tmp_path / ".age.toml").exists()
