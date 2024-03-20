aacbf8462ac5c2390ae46be4000b4c6d55092de3
Kazuya Takei <myself@attakei.net>
2024-03-20 17:54:12 +0900

    ci: Run 'cargo build' before run pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,23 @@
 """Common test contexts."""
 
 from pathlib import Path
+from subprocess import PIPE, run
 
 import pytest
+
+project_root = Path(__file__).parent.parent
+
+
+@pytest.fixture(scope="session", autouse=True)
+def build_test_target():
+    """Generate age binary for testing."""
+    run(["cargo", "build"], stdout=PIPE, stderr=PIPE, cwd=project_root)
 
 
 @pytest.fixture
 def root() -> Path:
     """Path-object of project root."""
-    return Path(__file__).parent.parent
+    return project_root
 
 
 @pytest.fixture
