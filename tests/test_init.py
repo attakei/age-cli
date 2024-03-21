cddd05aba5f5cdccd7fb1603c3a6e21b72f1a7a2
Kazuya Takei <myself@attakei.net>
2024-03-21 11:22:11 +0900

    ci: Add test for preset args in 'init'

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,11 +1,38 @@
 """Test case for ``age init``."""
 
 from pathlib import Path
-from subprocess import PIPE, run
+
+import pytest
 
 
-def test_it(target_bin: str, tmp_path: Path):
+def test_it(cmd, tmp_path: Path):
     """'init' command generate config file."""
-    proc = run([target_bin, "init"], stdout=PIPE, stderr=PIPE, text=True, cwd=tmp_path)
+    proc = cmd("init")
     assert proc.returncode == 0
-    assert (tmp_path / ".age.toml").exists()
+    #
+    age_toml = tmp_path / ".age.toml"
+    assert age_toml.exists()
+    age_text = age_toml.read_text()
+    assert 'current_version = "0.0.0"' in age_text
+
+
+@pytest.mark.parametrize(
+    "presets,files",
+    [
+        (["rust"], ["Cargo.toml"]),
+        (["python"], ["pyproject.toml"]),
+        (["rust", "python"], ["Cargo.toml", "pyproject.toml"]),
+    ],
+)
+def test_with_preset(cmd, tmp_path: Path, presets: list[str], files: list[str]):
+    """'init' command generate config file."""
+    args = ["init", "--preset"] + presets
+    proc = cmd(*args)
+    assert proc.returncode == 0
+    #
+    age_toml = tmp_path / ".age.toml"
+    assert age_toml.exists()
+    age_text = age_toml.read_text()
+    assert 'current_version = "0.0.0"' in age_text
+    for f in files:
+        assert f'[[files]]\npath = "{f}"' in age_text
