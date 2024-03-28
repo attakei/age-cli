4f3bb3df72aa27ee9c6c36cca36af59df4dae860
Kazuya Takei <myself@attakei.net>
2024-03-28 22:16:33 +0900

    ci: Add tests for major/minor/patch subcommands

---
+++ tests/test_patch.py
@@ -0,0 +1,39 @@
+"""Test case for ``age update``."""
+
+import shutil
+from pathlib import Path
+from subprocess import CompletedProcess, run
+
+import pytest
+
+root = Path(__file__).parent
+
+
+def get_env_dirs(name: str):  # noqa: D103
+    paths = [p for p in (root / name).glob("*") if p.is_dir()]
+    return {"argvalues": paths, "ids": [f"{p.parent.name}/{p.name}" for p in paths]}
+
+
+@pytest.mark.parametrize(
+    "env_path",
+    **get_env_dirs("return-1"),
+)
+def test_invalid_env(cmd, env_path: Path, tmp_path: Path):
+    """Run test cases on env having invalid configuration."""
+    print(env_path)
+    shutil.copytree(env_path, tmp_path, dirs_exist_ok=True)
+    proc: CompletedProcess = cmd("patch")
+    assert proc.returncode != 0
+
+
+@pytest.mark.parametrize(
+    "env_path",
+    **get_env_dirs("for-patch"),
+)
+def test_valid_env(cmd, env_path: Path, tmp_path: Path):
+    """Run test cases on env having valid files."""
+    shutil.copytree(env_path / "before", tmp_path, dirs_exist_ok=True)
+    proc: CompletedProcess = cmd("patch")
+    assert proc.returncode == 0
+    diff = run(["diff", "--recursive", str(tmp_path), str(env_path / "after")])
+    assert diff.returncode == 0
