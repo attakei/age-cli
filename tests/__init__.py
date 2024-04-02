86d6abd8bd54151e10a16d27a090e3a3ff00be13
Kazuya Takei <myself@attakei.net>
2024-04-03 00:28:54 +0900

    ci: Refactor tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,6 +4,10 @@ from pathlib import Path
 root = Path(__file__).parent
 
 
-def get_env_dirs(name: str):  # noqa: D103
+def get_env_dirs(name: str):
+    """Return structure of folder-path as test pattern.
+
+    .. note:: This is for parametrize.
+    """
     paths = [p for p in (root / name).glob("*") if p.is_dir()]
     return {"argvalues": paths, "ids": [f"{p.parent.name}/{p.name}" for p in paths]}
--- tests/test_update.py
+++ tests/test_update.py
@@ -6,17 +6,12 @@ from subprocess import CompletedProcess, run
 
 import pytest
 
-root = Path(__file__).parent
-
-
-def get_env_dirs(name: str):  # noqa: D103
-    paths = [p for p in (root / name).glob("*") if p.is_dir()]
-    return {"argvalues": paths, "ids": [f"{p.parent.name}/{p.name}" for p in paths]}
+from . import get_env_dirs
 
 
 def test_invalid_args(cmd, tmp_path: Path):
     """Run test case on env having valid files, but command argument is invalid."""
-    env_path = next((root / "return-0").glob("*"))
+    env_path = get_env_dirs("return-0")["argvalues"][0]
     shutil.copytree(env_path, tmp_path, dirs_exist_ok=True)
     proc: CompletedProcess = cmd("update", "-1")
     assert proc.returncode != 0
